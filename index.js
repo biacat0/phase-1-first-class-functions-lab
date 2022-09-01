b810b2e5b5b6e884a73b4a609d21814b05a7af2c
June Biadog <jb.biadog@gmail.com>
2022-09-01 17:14:11 -0400

    commit tree 95f5b751adda9cdfab0dc566e1a5acf22e81086f

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers() {
+    return ['Antonia', 'Nuru']
+}
+function returnLastTwoDrivers() {
+    return ['Amari', 'Mo']
+}
+function selectingDrivers() {
+    return [returnFirstTwoDrivers, returnLastTwoDrivers]
+}
